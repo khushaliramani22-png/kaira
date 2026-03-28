66663902fddb7abd64ad3f456d12042c74f82188
khushali ramani <khushaliramani22@gmail.com>
2026-03-28 13:06:36 +0530

    upadte orders page

--- src/app/api/admin/settings/orders/route.js
+++ src/app/api/admin/settings/orders/route.js
@@ -1,4 +1,4 @@
-import { supabaseAdmin } from "@/lib/adminsupabaseadmin";
+import { supabaseAdmin } from "@/lib/adminsupabase";
 import { NextResponse } from "next/server";
 
 export async function GET() {
